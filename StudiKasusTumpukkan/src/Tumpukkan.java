e11bb14528d06a99ff1fc33e3060d27eca92af3f
fanisa <fanisa@LAPTOP-MB8IH22D>
2020-12-26 12:21:19 +0700

    Initial Commit

---
+++ StudiKasusTumpukkan/src/Tumpukkan.java
@@ -0,0 +1,49 @@
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fanisa
+ */
+public class Tumpukkan {
+    public static void main(String[] args){
+                Stack st = new Stack();
+                System.out.println("Stack: "+st);
+                showPush(st, 7);
+                showPush(st, 90);
+                showPush(st, 15);
+                showPop(st);
+                showPop(st);
+                showPop(st);
+                try {
+                    showPop(st);
+                }catch (EmptyStackException e){
+                    System.out.println("Error, pesan: "+e.getMessage());
+}
+    }
+        static void showPush(Stack st, int a){
+            st.push(new Integer(a));
+            System.out.println("Push (" +a+ ")");
+            System.out.println("Stack: "+st);
+        }
+        
+        static void showPop(Stack st){
+            System.out.print("Pop -> ");
+            Integer a = (Integer) st.pop();
+            System.out.println(a);
+            System.out.println("Stack: " + st);
+            
+   
+                }
+            }
+        
+        
+    
+
